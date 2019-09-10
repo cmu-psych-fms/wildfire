a72a4d8c5100616e5363a8756a7024c3ece37047
Shawn Betts <sabetts@andrew.cmu.edu>
2019-09-10 00:41:33 -0700

    Add a dummy model written in python.

---
+++ model_client.py
@@ -0,0 +1,194 @@
+import random
+import socketio
+
+class Model(object):
+    def __init__(self):
+        self.sio = socketio.Client()
+        self.sio.on('connect', self.on_connect)
+        self.sio.on('start', self.on_start)
+        self.sio.on('serverUpdate', self.on_server_update)
+        self.sio.on('join', self.on_player_join)
+        self.sio.on('part', self.on_player_part)
+        self.sio.on('reset', self.on_game_reset)
+
+    def connect_to_server(self, url):
+        self.sio.connect(url)
+
+    def on_connect(self):
+        print('Connected to server.')
+        self.sio.emit('greet', {'mode': 'player'});
+
+    def update_player(self, player_id, data):
+        self.players[player_id]['alive'] = data[0]
+        self.players[player_id]['x'] = data[1]
+        self.players[player_id]['y'] = data[2]
+        self.players[player_id]['angle'] = data[3]
+        self.players[player_id]['speed'] = data[4]
+        self.players[player_id]['turnFlag'] = data[5]
+        self.players[player_id]['water'] = data[6]
+
+    def on_start(self, data):
+        print('start')
+        self.id = data['id']
+        self.server_game_tick = None
+        self.map = data['map']
+        self.players = {}
+        self.last_confirmed_movement_sequence = None
+        self.movement_request_sequence = 0
+        for player_id in data['players']:
+            self.players[player_id] = {}
+            self.update_player(player_id, data['players'][player_id])
+        # Start the client game loop
+        self.sio.start_background_task(self.client_game_loop)
+
+    # Sample server update
+    # {'t': 48,
+    #  'p': {'e778c090-d0fb-11e9-82c9-1f5dc901c465': [1, 4000, 4000, 100, 0, 0, 50]},
+    #  'm': [[90609, 2], [68568, 2], [81422, 2]],
+    #  'f': [[2, {'x': 209, 'y': 226, 'level': 1}], [1, {'x': 210, 'y': 226, 'level': 1}], [1, {'x': 209, 'y': 227, 'level': 1}], [1, {'x': 209, 'y': 225, 'level': 1}], [2, {'x': 168, 'y': 171, 'level': 1}], [1, {'x': 168, 'y': 172, 'level': 1}], [1, {'x': 167, 'y': 171, 'level': 1}], [1, {'x': 168, 'y': 170, 'level': 1}], [2, {'x': 222, 'y': 203, 'level': 1}], [1, {'x': 223, 'y': 203, 'level': 1}], [1, {'x': 222, 'y': 204, 'level': 1}]],
+    #  'r': [], 'wp': [], 'lk': {'seq': None, 'tick': None}}
+
+    def on_server_update(self, data):
+        # The server updates the game state every 15ms but the server
+        # updates are only sent once every 100ms. This is done to
+        # reduce the amount of data sent to the clients. Depending on
+        # the pace of change and the game's features, more frequent
+        # updates may be necessary. For the current version of
+        # wildfire, 100ms seems to be fine.
+        #
+        # To create the illusion of smooth flying for humans, the web
+        # client uses linear interpolation to smoothly move the other
+        # airplanes' positions between updates.
+        # Update the state of the players.
+        # print('server update', data)
+        for player_id in data['p']:
+            self.update_player(player_id, data['p'][player_id])
+        # Update the current game tick
+        self.server_game_tick = data['t']
+        # Changes to the map. Currently this is exclusively tiles
+        # being replaced with ash after a fire has burned it.
+        for m in data['m']:
+            self.map[m[0]] = m[1]
+        # Changes to the fire list.
+        for f in data['f']:
+            if f[0] == 1: # add fire
+                self.map['fire'].append(f[1])
+            elif f[0] == 2: # delete fire
+                for i in range(len(self.map['fire'])):
+                    if self.map['fire'][i]['x'] == f[1]['x'] and self.map['fire'][i]['y'] == f[1]['y']:
+                        del self.map['fire'][i]
+                        break
+        # Changes to the retardant list.
+        for r in data['r']:
+            if r[0] == 0: # update retardant
+                for i in range(len(self.map['retardant'])):
+                    if self.map['retardant'][i]['x'] == f[1]['x'] and self.map['retardant'][i]['y'] == f[1]['y']:
+                        self.map['retardant'][i] = r[1]
+            if r[0] == 1: # add retardant
+                self.map['retardant'].append(r[1])
+            elif r[0] == 2: # delete retardant
+                for i in range(len(self.map['retardant'])):
+                    if self.map['retardant'][i]['x'] == f[1]['x'] and self.map['retardant'][i]['y'] == f[1]['y']:
+                        del self.map['retardant'][i]
+                        break
+        # Changes to the way-point list
+        for wp in data['wp']:
+            if wp[0] == 1: # add a waypoint
+                self.map.wayPoints.append(f[1])
+        # The most recent movement sequence that the server has
+        # received as of this server update. This is needed by the web
+        # client to provide the illusion of lag-free movement for
+        # human players. See the web client source code for how it is
+        # used.
+        self.last_confirmed_movement_sequence = data['lk']['seq']
+        if 'vp' in data:
+            # The amount of the map that is currently visible (aka
+            # "viewport") has changed. This is an experimental feature
+            # I was playing around with at one point. At the beginning
+            # of the game, the visible part of the map is small, but
+            # over time the map grows exposing more land that the
+            # fires can burn.
+            #
+            # Every time the map grows, the server includes the new
+            # visible portion of the map in the next update.
+            pass
+
+    def on_player_join(self, data):
+        print('player join', data['id'])
+        # Player data will be filled in on the next server update.
+        self.players[data['id']] = {}
+
+    def on_player_part(self, data):
+        print('player part', data['id'])
+        # The player has disconnected from the game. Clean up their
+        # data.
+        del self.players[data['id']]
+
+    def on_game_reset(self, data):
+        print('game reset');
+        # A player has requested that the game be reset to the
+        # starting state.
+        self.on_start(data)
+
+
+    def send_movement_request(self, turn, thrust, dump, waypoints=[]):
+        # Tell the server you'd like to move your water bomber. This
+        # request happens 60 times a second--once per game tick. The
+        # request includes:
+        #
+        # * the state of the turning (l=left, r=right, 0=fly straight),
+        #
+        # * the state of the "thrust" (f=speeding up, s=slowing down,
+        # 0=unchanged)
+        #
+        # * the dump flag (1=dumping retardant, 0=not dumping)
+        #
+        # * A list of way points that you'd like to add (currenty
+        # there is only 1=water). So if you want to place a water
+        # waypoint, then waypoints should be [1].
+        self.sio.emit('movementRequest', [self.movement_request_sequence,
+                                          turn,
+                                          thrust,
+                                          dump,
+                                          waypoints])
+        self.movement_request_sequence += 1
+
+    def client_game_loop(self):
+        # The client game loop sends movement requests at 60 frames
+        # per second. It is these movement requests that make the
+        # player's airplane move. Each movement request that the
+        # server gets will step the airplane's movement by one
+        # increment.
+        #
+        # In other words, without these movement requests the airplane
+        # stands still. Obviously there is an easy game exploit: send
+        # more movement requests than 60 per second and you can travel
+        # around the map faster and put out more fires.
+        #
+        # If this was a real online game, the server would have
+        # anti-cheating measures, such as ignoring the extra movement
+        # requests.
+        state = {'turn': 0,
+                 'thrust': 0,
+                 'dump': 0}
+        waypoints = []
+        ticks = 0
+        while True:
+            # Do something random every 3rd of a second
+            if ticks % 20 == 0:
+                k = random.choice(list(state.keys()))
+                if k == 'turn':
+                    state[k] = random.choice([0,'l', 'r'])
+                elif k == 'thrust':
+                    state[k] = random.choice([0,'f', 's'])
+                elif k == 'dump':
+                    state[k] = random.choice([0, 1])
+            m.send_movement_request(state['turn'], state['thrust'], state['dump'], waypoints)
+            self.sio.sleep(1/60.0)
+            ticks += 1
+
+
+if __name__ == '__main__':
+    m = Model()
+    m.connect_to_server('http://localhost:3000')
+
